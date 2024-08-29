d783fb8e55f29d399d2ac5f8a89754d187e77cfc
Harsh Sharma <114438275+hrshh200@users.noreply.github.com>
2024-08-29 23:43:58 +0530

    Update Header.js

--- frontend/src/components/Dashboard/Header.js
+++ frontend/src/components/Dashboard/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <h1>BLOG</h1>
+            <h1>BLOG STUDIO</h1>
             <nav>
                 <ul>
                     <li><a href="#dashboard">Login</a></li>
